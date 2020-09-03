9cf65f532e7d9c8b8b2ce9bcb8c1c12739b4b14b
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-09-03 22:14:24 +0300

    Add files via upload

---
+++ Week2/tgif.py
@@ -0,0 +1,53 @@
+def main():
+    month =1
+    for i in range(3, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month +=1
+    for i in range(7, 29, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(7, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(4, 31, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(2, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(6, 31, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(4, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(1, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(5, 31, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(3, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(7, 31, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+    month += 1
+    for i in range(5, 32, 7):
+        print(i, ".", end="", sep="")
+        print(month, ".", sep="")
+
+if __name__ == "__main__":
+    main()
+
