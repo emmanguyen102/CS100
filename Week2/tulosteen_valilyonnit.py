0aefc2558a3da4e9f12c386acbfa0d04ca61b1c7
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-09-03 18:11:57 +0300

    Add files via upload

---
+++ Week2/tulosteen_valilyonnit.py
@@ -0,0 +1,7 @@
+def main():
+    name = str(input("Tell us your name: "))
+    print("Hey", name, end="")
+    print(", the printout formatting is going well!")
+
+if __name__ == "__main__":
+    main()
