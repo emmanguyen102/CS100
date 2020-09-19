34a473a2d4ca6473c0a9f84e2f34969414b84c17
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-09-19 22:06:48 +0300

    Update kolmion_kulmat.py

--- Week4/kolmion_kulmat.py
+++ Week4/kolmion_kulmat.py
@@ -1,8 +1,3 @@
-"""
-COMP.CS.100 Programming 1
-Hang Nguyen, hang.t.nguyen@tuni.fi, student id K429778
-Triangle's angle
-"""
 def calculate_angle(s1,s2=90):
     """
     Calculate the magnitude of 3rd angle based the other 2 angles
