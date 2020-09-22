8d3ba2074726d3583628e0b69332d6273d04e572
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-09-22 16:22:32 +0300

    Add files via upload

---
+++ Week5/lukusarjoja.py
@@ -0,0 +1,31 @@
+"""
+COMP.CS.100 Programming 1
+Hang Nguyen, hang.t.nguyen@tuni.fi, student id K429778
+Number series
+"""
+def is_even_num_ascending(l):
+    """
+    print the even numbers in ascending order
+    :param l: the biggest number
+    :return: list of number in ascending order
+    """
+    for i in range (0, l, 2):
+        print(i)
+    return i
+def is_even_num_descending(l):
+    """
+    print the event numbers in descending order
+    :param l: the biggest number
+    :return: in descending order
+    """
+    for i in range (l, -1, -2):
+        print(i)
+    return i
+
+def main():
+    is_even_num_ascending(101)
+    is_even_num_descending(100)
+
+
+if __name__ == "__main__":
+    main()
