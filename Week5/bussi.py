7203624d40415e14181998279b9a5b4b7290fdf7
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-09-23 11:18:18 +0300

    Update bussi.py

--- Week5/bussi.py
+++ Week5/bussi.py
@@ -1,9 +1,3 @@
-"""
-COMP.CS.100 Programming 1
-Hang Nguyen, hang.t.nguyen@tuni.fi, student id K429778
-5.6.2: Bus Time Table
-"""
-
 def main():
     bus_schedule = [630, 1015, 1415, 1620, 1720, 2000]
     user_time = int(input("Enter the time (as an integer): "))
