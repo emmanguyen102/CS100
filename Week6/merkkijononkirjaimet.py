f464192e8ed8d75d8c732dc92286ca6b198b36e0
Emma <57014399+emmanguyen102@users.noreply.github.com>
2020-10-01 21:43:12 +0300

    Update merkkijononkirjaimet.py

--- Week6/merkkijononkirjaimet.py
+++ Week6/merkkijononkirjaimet.py
@@ -1,13 +1,3 @@
-"""
-COMP.CS.100 Ohjelmointi 1 / Programming 1
-Name:       Hang Nguyen
-Email:      hang.t.nguyen@tuni.fi
-Student Id: K429778
-
-A program that asks a user for a word and tells
-how many vowels and consonants the word contains
-"""
-
 def main():
     str_input = str(input("Enter a word: "))
     vowel_count=0
